74a6bc5f42935ead33acd710dd4fb4b334e71226
Lalo <aguerre.lara@gmail.com>
2026-02-25 21:24:36 +0100

    Creating clock

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
-        log_state()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        # Limitar a 60 FPS y obtener delta time
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
